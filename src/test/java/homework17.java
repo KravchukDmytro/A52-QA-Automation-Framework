8ea709906b12303e9e2fc30f46356c6c6ad67967
KravchukDmytro <de440003@gmail.com>
2024-01-28 19:13:06 -0500

    homework 17

--- src/test/java/homework17.java
+++ src/test/java/homework17.java
@@ -12,17 +12,35 @@ public class homework17 extends BaseTest{
     public void addSongToPlaylist() {
 
         login("dmytro.kravchuk@testpro.io", "Fr440003");
-       // WebElement addNewPlaylist = driver.findElement(By.cssSelector(""));
 
-WebElement addPlaylistButton = driver.findElement(By.cssSelector("[data-testid='sidebar-create-playlist-btn']"));
-addPlaylistButton.click();
-WebElement addPlaylist = driver.findElement(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']"));
- addPlaylist.click();
- WebElement newPlaylistName = driver.findElement(By.cssSelector("i[title='Create a new playlist']"));
-String namePlaylist = UUID.randomUUID().toString();
+
+//WebElement addPlaylistButton = driver.findElement(By.cssSelector("[data-testid='sidebar-create-playlist-btn']"));
+//addPlaylistButton.click();
+//WebElement addPlaylist = driver.findElement(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']"));
+ //addPlaylist.click();
+ //WebElement newPlaylistName = driver.findElement(By.cssSelector("i[title='Create a new playlist']"));
+//String namePlaylist = UUID.randomUUID().toString();
 
 
         WebElement search = driver.findElement(By.cssSelector("input[type='search']"));
+        search.sendKeys("F");
+ WebElement brand = driver.findElement(By.cssSelector("[class='brand']"));
+ brand.click();
+
+WebElement allViewButton = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
+allViewButton.click();
+WebElement clickSong = driver.findElement(By.cssSelector("#songResultsWrapper > div > div > div.item-container > table > tr"));
+clickSong.click();
+WebElement addToButton = driver.findElement(By.cssSelector("[class='btn-add-to']"));
+addToButton.click();
+WebElement playlistNameNew = driver.findElement(By.xpath("//*[@id='queueWrapper']/header/div[3]/div/section[2]/form/input"));
+playlistNameNew.sendKeys("HW17");
+WebElement submitButton = driver.findElement(By.xpath("//*[@id='songResultsWrapper']/header/div[3]/div/section[2]/form/button"));
+submitButton.click();
+
+
+
+
 
     }
 
