a5a67e81859ba971ecb193c2b962a937ebbc7b02
KravchukDmytro <de440003@gmail.com>
2024-02-03 17:29:13 -0500

    playlist deleted

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,30 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
 public class BaseTest {
      protected WebDriver driver = null;
+     protected WebDriverWait wait = null;
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
     @BeforeMethod
-    public void setUpDriver() {
+    @Parameters("baseUrl")
+    public void setUpDriver(String url) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
+      // String url = "https://qa.koel.app/";
         driver.get(url);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
     }
 
     @AfterMethod
